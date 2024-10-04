04463874685328eb765d1b4bcf6b37390b7bd352
Abel-G <abeljeb@gmail.com>
2024-10-04 23:47:34 +0300

    adding static structure

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div>
+      <h1>Weather Update</h1>
+      <div>
+        <input type="text" />
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function WeatherCard() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h1>16°</h1>
+        </div>
+        <div> 
+          <h3>Sunny</h3>
+        </div>
+      </div>    
+      <div>
+        <div>
+          <div>
+            <h1>London</h1>
+          </div>
+          <div>
+            <h3>Monday, 20 January</h3> 
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Feels Like</h3>
+            <h3>16°</h3>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Wind</h3>
+            <h3>10 km/h</h3>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Humidity</h3>
+            <h3>15%</h3>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>Rain</h3>
+            <h3>N/A</h3>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>  
+    </div>
+  )
+}
+
+export default WeatherCard
