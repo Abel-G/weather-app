07dee42172ce630f3c0feaea09f22425df50cc35
Abel-G <abeljeb@gmail.com>
2024-10-05 00:03:25 +0300

    fetching data from openweatherapi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,26 @@ import WeatherCard from './components/WeatherCard'
 import SearchBar from './components/SearchBar'
 
 function App() {
+  const [data, setData] = useState({});
   
+  const fetchWeatherData = (location) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${import.meta.env.VITE_API_KEY}`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error fetching weather data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  };
   return (
     <div className="App">
       <SearchBar /> 
