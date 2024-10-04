978c29b1a311bd655cc28a3bb9684d47a95d0558
Abel-G <abeljeb@gmail.com>
2024-10-04 23:57:12 +0300

    search bar update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import search from '../assets/search.png'
+function SearchBar({ onSearch }) {
+  const inputRef = useRef(); 
+  const [location, setLocation] = useState('');
 
-function SearchBar() {
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      onSearch(location);
+      setLocation(''); 
+    }
+  };
+
+  const handleIconClick = () => {
+    onSearch(location);
+    setLocation(''); 
+  };
   return (
     <div>
       <h1>Weather Update</h1>
       <div>
-        <input type="text" />
-        <img src="" alt="" />
+        <input 
+        ref={inputRef}
+        value={location}
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder='Enter Location'
+        type='text'
+        className='outline:none' />
+        <img 
+          src={search}
+          alt="Search"
+          className=' size-10 ml-2'
+          onClick={handleIconClick} />
       </div>
     </div>
   )
